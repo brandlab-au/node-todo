bbc7f5b072285e2fa9b98c068f1e80ad9f4360ad
Jon Mckenzies <jon@brandlab.co.uk>
2016-09-13 14:24:16 -0500

    sa

---
+++ config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+<<<<<<< HEAD
+    remoteUrl : '',
+    localUrl: 'mongodb://localhost:27017/dummydb'
+=======
+    remoteUrl : 'mongodb://jonathon:9911ssaa@ec2-54-200-209-160.us-west-2.compute.amazonaws.com:27017/dummyDB',
+    localUrl: 'mongodb://localhost/meanstacktutorials'
+>>>>>>> 54db03b56e9a468ef1d32926471cd87dd3261b68
+};
+
+
+
+//'mongodb://{NEW USERNAME}:{NEW PASSWORD}@{EC2 URL}:{PORT}/dummyDB'
+
